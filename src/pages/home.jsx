d3c5e3ac2deb325106a67e6a31da31bc18fc6920
Tayyab Siraj <tayyab.siraj@identifymobile.com>
2025-01-10 12:11:49 +0500

    started working on the ui

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,45 @@
 import "../assets/css/home.css";
-import Header from '../reuseable-components/header'
-import Footer from '../reuseable-components/footer'
-
+import Header from "../reuseable-components/header";
+import Footer from "../reuseable-components/footer";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
 
 function Home() {
-  return(
+  return (
     <>
-    <Header/>
-    <h1>Homeeeeeeeeeeeeeeeeee</h1>
-    <Footer/>
+      <Header />
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide className="slide-1">
+          <div className="slider-text">
+            <h6>Largest Online Madrasah</h6>
+            <h1>Islamic Online Mudarsa</h1>
+            <p>
+              Islamic online madrasah is an ISO certified educational
+              institution of international standards
+            </p>
+            <button className="btn">Find The Courses</button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="slide-2">
+          <div className="slider-text">
+            <h6>Largest Online Madrasah</h6>
+            <h1>Islamic Online Mudarsa</h1>
+            <p>
+              Islamic online madrasah is an ISO certified educational
+              institution of international standards
+            </p>
+            <button className="btn">Find The Courses</button>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </>
-  )
+  );
 }
 
 export default Home;
--- src/reuseable-components/header.jsx
+++ src/reuseable-components/header.jsx
@@ -4,45 +4,107 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      {/* top bar*/}
+      <div className="topbar">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
+          <div className="row align-items-center">
+            <div className="col-md-6">
+              <div className="top-bar-left">
+                <ul className="d-flex gap-5 justify-content-start align-items-center">
+                  <li>
+                    <i className="fa-regular fa-envelope"></i>
+                    <a href="">Info@example.com</a>
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-map-pin"></i>
+                    <a href="">Suite 80 Golden Street Germeney</a>
+                  </li>
+                  <li>
+                    <img
+                      src="https://themazine.com/newwp/alquran/wp-content/themes/alquran/assets/images/icons/bismillah.png"
+                      alt=""
+                    />
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="top-bar-right">
+                <ul className="d-flex gap-4 justify-content-end align-items-center">
+                  <li>
+                    <i className="fa-regular fa-sun"></i>
+                    <a href="">Sunrise At: 5:15 AM </a>
+                  </li>
+                  <li>
+                    <i className="fa-regular fa-moon"></i>
+                    <a href="">Sunset At: 4:50 PM</a>
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-phone"></i>
+                    <a href="">Let’s Talk +123-456-789</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* navbar */}
+      <nav className="navbar navbar-expand-lg ">
+        <div className="container-fluid">
+          <img
+            src="https://themazine.com/newwp/alquran/wp-content/uploads/2023/12/logo.svg"
+            alt="Logo"
+            width="160"
+            height="40"
+            className="d-inline-block align-text-top"
+          />
+
           <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav mx-auto">
               <li className="nav-item">
                 <Link className="nav-link active" to="/home">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about-us">
-                  About
+                <Link className="nav-link " to="/about-us">
+                  About Us
                 </Link>
               </li>
-
               <li className="nav-item">
-                <Link className="nav-link" to="/contact">
-                  Contact
+                <Link className="nav-link " to="/courses">
+                  Courses
                 </Link>
               </li>
-
               <li className="nav-item">
-                  <Link className="nav-link" to="/courses">Courses</Link>
+                <Link className="nav-link " to="/contact">
+                  Contact Us
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  Blog
+                </Link>
               </li>
             </ul>
+
+            <button className="btn" type="button">
+              Join Now
+            </button>
           </div>
         </div>
       </nav>
